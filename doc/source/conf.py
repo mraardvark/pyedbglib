0aa12e10fde9e42005aac925f4ac2370ed5c1ca8
mraardvark <juddbard@gmail.com>
2020-12-08 17:10:52 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 autodoc_mock_imports = []
-for mod in ['cython', 'hidapi', 'pyserial']:
+for mod in ['cython', 'hidapi', 'cyhidapi', 'pyserial']:
     try:
         importlib.import_module(mod)
     except ImportError:
